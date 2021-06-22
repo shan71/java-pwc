6bd9a89170452170b7dbde5adea23f44a11246e4
Administrator <Administrator@172.20.0.60>
2021-06-22 13:01:02 +0100

    mvc app uploaded

---
+++ spring-mvc-app/src/main/java/com/demo/spring/DemoController.java
@@ -0,0 +1,24 @@
+package com.demo.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	
+	@RequestMapping(path="/test",method = RequestMethod.GET)
+	public String getPage() {
+		System.out.println("inside Controller...");
+		
+		return "/WEB-INF/pages/register.jsp";
+	}
+	
+	@RequestMapping(path="/info",method = RequestMethod.GET)
+	@ResponseBody
+	public String info() {
+		return "This is a Spring REST Service";
+	}
+
+}
