1c441d995296a752e0c81aae38545e370da82c74
Administrator <Administrator@172.20.0.60>
2021-06-29 06:29:44 +0100

    emp service dao added

---
+++ emp-service2/src/main/java/com/demo/rest/EmpDao.java
@@ -0,0 +1,17 @@
+package com.demo.rest;
+
+import java.util.List;
+
+import com.demo.rest.entity.Emp;
+
+public interface EmpDao {
+	public Emp findById(int id);
+
+	public List<Emp> listAll();
+
+	public String save(Emp e);
+
+	public String delete(int id);
+
+	public String update(Emp e);
+}
